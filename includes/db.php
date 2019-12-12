faa97dfe2558cc84a1ed45a4bef9a22b05517f05
Vinayak Mandrekar <k.vinayak05@gmail.com>
2019-12-12 21:06:18 +0530

    0.0.0 dynamic posting+ Search query in progresss

---
+++ includes/db.php
@@ -0,0 +1,19 @@
+<?php
+
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_password'] = "";
+$db['db_name'] = "cms";
+
+foreach($db as $key=>$value){
+  define(strtoupper($key),$value);
+}
+//error dont use "" for DB_* constants
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+if($connection){
+  echo "connection sucess";
+}
+
+
+ ?>
---
+++ includes/sidebar.php
@@ -0,0 +1,83 @@
+<div class="col-md-4">
+ <?php
+ if(isset($_POST["submit"])){
+
+   echo $search = $_POST['search'];
+
+  $query = "SELECT * FROM posts WHERE post_tag LIKE '%$search%' ";
+  $search_query = mysqli_query($connection,$query);
+
+  if(!$search_query){
+    die("QUERY failed".mysqli_error($connection));
+  }else{
+
+     $count = mysqli_num_rows($search_query);
+     if($count == 0 ){
+       echo "found_nothing";
+     }
+
+
+  }
+
+
+ } else {}
+
+  ?>
+    <!-- Blog Search Well -->
+
+    <div class="well">
+        <h4>Blog Search</h4>
+        <form class="" action="" method="post">
+          <div class="input-group">
+            <input name="search" type="text" class="form-control">
+            <span class="input-group-btn">
+                <button class="btn btn-default" name="submit" type="submit">
+                <span class="glyphicon glyphicon-search"></span>
+            </button>
+            </span>
+        </div>
+      </form>
+        <!-- /.input-group -->
+    </div>
+
+    <!-- Blog Categories Well -->
+    <div class="well">
+        <h4>Blog Categories</h4>
+        <div class="row">
+            <div class="col-lg-6">
+                <ul class="list-unstyled">
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.col-lg-6 -->
+            <div class="col-lg-6">
+                <ul class="list-unstyled">
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.col-lg-6 -->
+        </div>
+        <!-- /.row -->
+    </div>
+
+    <!-- Side Widget Well -->
+    <div class="well">
+        <h4>Side Widget Well</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+    </div>
+
+</div>
